681c418c1e84ce5ee64762a950e9f210d65df0cd
Lucas Lencinas <lllencinas@gmail.com>
2015-09-01 20:35:26 -0300

    Lista

--- Lista/app.js
+++ Lista/app.js
@@ -2,17 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var alumnos = [];
+var profesores = [];
+
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.on('profesorSeConecta', function(socket){
-  console.log('an alumno se conecta');
+  console.log('an profesor se conecta');
 	
 });
 
 io.on('alumnoSeConecta', function(socket){
-  console.log('a profesor se conecta');
+  console.log('a alumno se conecta');
 	
 });
 
