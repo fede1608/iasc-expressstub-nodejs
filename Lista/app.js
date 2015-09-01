573f54e8074022db07a2fc2bfedbc5c125cce109
Lucas Lencinas <lllencinas@gmail.com>
2015-09-01 20:57:47 -0300

    lista listeners

--- Lista/app.js
+++ Lista/app.js
@@ -5,17 +5,37 @@ var io = require('socket.io')(http);
 var alumnos = [];
 var profesores = [];
 
+var consultas = [];
+
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('profesorSeConecta', function(socket){
+io.on('profesorSeConecta', function(data){
   console.log('an profesor se conecta');
 	
 });
 
-io.on('alumnoSeConecta', function(socket){
+io.on('alumnoSeConecta', function(data){
+  console.log('a alumno se conecta');
+	
+});
+
+io.on('alumnoEscribe', function(data){
+  console.log('a alumno escribe');
+  console.log(data);
+  var consulta = Consulta(data, alumno);
+  for (profesor in profesores){
+  	profesor.socket.emit('nuevaConsulta', consulta);
+  };
+  for (alumno in alumnos){
+  	alumno.socket.emit('nuevaConsulta', consulta);
+  };
+
+});
+
+io.on('profesorResponde', function(data){
   console.log('a alumno se conecta');
 	
 });
