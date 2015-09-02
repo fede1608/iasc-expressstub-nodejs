d7f54c0e7bf3783a5b09f656b48777c210df0047
fnc <fnc@git.com>
2015-09-01 21:30:04 -0300

    prof

--- profesor.js
+++ profesor.js
@@ -1,20 +1,53 @@
+//==========
+//===EH LEE LA DOCUMENTACIÓN ACA=====
+//==http://expressjs.com/guide/routing.html===
+//==Para más detalles de la API MIRA ACA===
+//==http://expressjs.com/4x/api.html
 var express = require('express');
+var http = require('http');
 var app = express();
-var http = require('http').Server(app);
 
 
-http.get("http://localhost:3000/profesorSeConecta", function(res) {
-  console.log("Got response: " + res.statusCode);
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
+var server = app.listen(4000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
+//while(true) {
+  produce();
+//}
+function produce() {
+  console.log('voy');    
+  http.get('http://localhost:3000/profesorSeConecta', function(res){
+    console.log('hola');
+consulta();	    
+  });
+  //sleep(3000);
+}
 
+function consulta(){
 
+app.get('/nuevaConsulta', function (req, res) {
+  res.send('respuestaAPregunta');
+});
 
+}
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+            break;
+          }
+    }
+}
 
 
 
