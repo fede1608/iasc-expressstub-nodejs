1e13aa9c1791052bd8ab828b8f510a2ce6110232
jonac22 <jonathan.corallo92@gmail.com>
2015-09-02 00:21:43 -0300

    Refactor y logs

--- lista.js
+++ lista.js
@@ -1,13 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var colors = require('colors');
 var client = require('http');
-var address = "http://localhost"
+var _ = require('underscore');
+var address = "http://localhost";
 
-var alumnos = [];
-var profesores = [];
-
-var consultas = [];
+var students = [];
+var professors = [];
+var posts = [];
 
 //unirest
 app.get('/', function(req, res){
@@ -15,34 +15,37 @@ app.get('/', function(req, res){
 });
 
 app.get('/profesorSeConecta', function(req, res){
-  console.log('Un profesor se conecta');
-  console.log('Port: ' + req.query.port)
-  profesores.push(req.query.port);
-	res.send('OK');
+  addObserver(('A professor has connected' + req.query.port).blue, professors, req.query.port, res);
 });
 
 app.get('/alumnoSeConecta', function(req, res){
-  	console.log('un alumno se conecta');
-    console.log('Port: ' + req.query.port);
-    alumnos.push(req.query.port);
-	res.send('OK');
+  addObserver(('A student has connected, listening on ' + req.query.port).magenta, students, req.query.port, res);
 });
+
+function addObserver(logMessage, observers, port, res){
+  console.log(logMessage);
+  if(!_.contains(observers, port)){
+    observers.push(port);
+  }
+  console.log('current observers:', observers);
+  res.send('OK');
+}
 
 app.get('/alumnoEscribe', function(req, res){
   console.log('a alumno escribe');
   console.log(req.query);
   var consulta = req.query.consulta;//Consulta(data, alumno);
-  for (profesor in profesores){
+  for (profesor in professors){
   	//client.get(address+":"+profesor+"/nuevaConsulta?consulta="+consulta,function(){
 
   	//});
   };
-  for (alumno in alumnos){
+  for (student in students){
   	//client.get(address+":"+alumno+"/nuevaConsulta?consulta="+consulta,function(){
 
   	//});
   };
-res.send('OK');
+  res.send('OK');
 });
 
 app.get('/profesorResponde', function(data){
@@ -50,6 +53,25 @@ app.get('/profesorResponde', function(data){
 	res.send('OK');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+http.listen(port, function(){
+  console.log(('Mailing list is online on port ' + port).green, '[', new Date().toString().yellow, ']');
 });
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
